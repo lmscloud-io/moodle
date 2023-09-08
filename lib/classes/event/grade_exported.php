d09c6a0d10c403e4fb3f921d4766db4ea765e6fe
LMSCloud.io <contrib@lmscloud.io>
2023-09-08 19:36:29 +0100

    fixup

--- lib/classes/event/grade_exported.php
+++ lib/classes/event/grade_exported.php
@@ -54,18 +54,14 @@ abstract class grade_exported extends base {
      * @return string
      */
     public static function get_name() {
-<<<<<<< HEAD
         $component = 'gradeexport_' . self::get_export_type();
         if (get_string_manager()->string_exists('eventgradeexported', $component)) {
+            // Mdlcode assume-optional-next-line: $component pluginnames-gradeexport
             return get_string('eventgradeexported', $component);
         }
 
         // Fallback to generic name.
         return get_string('eventgradeexported', 'core_grades');
-=======
-        // Mdlcode assume: self::get_export_type() pluginnames-gradeexport
-        return get_string('eventgradeexported', 'gradeexport_'. self::get_export_type());
->>>>>>> Mdlcode 4.0
     }
 
     /**
